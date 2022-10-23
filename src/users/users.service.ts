82c9ca969e990600e1c52542f4a507dcaa30a6a4
Lucas Ferreira <lucaasferreirafl@gmail.com>
2022-10-23 13:02:33 -0300

    feat: crud users

---
+++ src/users/users.service.ts
@@ -0,0 +1,73 @@
+import { HttpException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../database/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create({ name, email, password }: CreateUserDto) {
+    const emailAlreadyExists = await this.prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (emailAlreadyExists)
+      throw new HttpException('Este e-mail já está em uso.', 400);
+
+    return this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+  }
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  async findOne(id: string) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) throw new HttpException('Usuário não encontrado.', 404);
+
+    return user;
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) throw new HttpException('Usuário não encontrado.', 404);
+
+    const emailAlreadyExists = await this.prisma.user.findFirst({
+      where: {
+        email: updateUserDto.email,
+        id: {
+          not: id,
+        },
+      },
+    });
+
+    if (emailAlreadyExists)
+      throw new HttpException('Este e-mail já está em uso.', 400);
+
+    return this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: updateUserDto,
+    });
+  }
+}
